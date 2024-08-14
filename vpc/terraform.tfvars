9f5afac765c69b06a29e8c95934f2ffd16c77bbb
ambigus9 <ambigus9@gmail.com>
2024-08-14 18:52:54 -0500

    Adding new resources like NAT Gateway and Internet Gateway

--- vpc/terraform.tfvars
+++ vpc/terraform.tfvars
@@ -4,7 +4,7 @@ tags = {
   "owner"       = "Miguel"
   "cloud"       = "AWS"
   "IAC"         = "Terraform"
-  "IAC_Version" = "1.9.2"
+  "IAC_Version" = "1.9.4"
   "project"     = "AWS-SAA-C03"
   "region"      = "virginia"
 }
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -26,6 +26,11 @@ resource "aws_subnet" "my_private_subnet" {
 resource "aws_route_table" "rt_public_subnet" {
   vpc_id = aws_vpc.vpc_lab.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
   tags = {
     Name = "RT-AWS-LABS-PUB"
   }
@@ -40,6 +45,11 @@ resource "aws_route_table_association" "rt_public_subnet_association" {
 resource "aws_route_table" "rt_private_subnet" {
   vpc_id = aws_vpc.vpc_lab.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.my_nat_gw.id
+  }
+
   tags = {
     Name = "RT-AWS-LABS-PRIV"
   }
@@ -48,4 +58,56 @@ resource "aws_route_table" "rt_private_subnet" {
 resource "aws_route_table_association" "rt_private_subnet_association" {
   subnet_id      = aws_subnet.my_private_subnet.id
   route_table_id = aws_route_table.rt_private_subnet.id
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.vpc_lab.id
+
+  tags = {
+    Name = "IGW-AWS-LABS"
+  }
+}
+
+resource "aws_nat_gateway" "my_nat_gw" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id     = aws_subnet.my_public_subnet.id
+
+  tags = {
+    Name = "NAT-AWS-LABS"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.my_igw]
+}
+
+resource "aws_eip" "my_eip" {
+  domain = "vpc"
+  depends_on = [aws_internet_gateway.my_igw]
+}
+
+resource "aws_network_acl" "my_network_acl" {
+  vpc_id = aws_vpc.vpc_lab.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  tags = {
+    Name = "NACL-AWS-LABS"
+  }
 }
